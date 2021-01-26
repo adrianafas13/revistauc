320a9275374f081e140dedc123b986c2e21dc65f
Adriana <adrianafas13@hotmail.com>
2021-01-26 14:16:16 -0400

    ultimo cambio a boceto de la maqueta

--- resources/views/layouts/userlayout.blade.php
+++ resources/views/layouts/userlayout.blade.php
@@ -184,10 +184,7 @@
 
     <div class="w3-bar">
         <div class="w3-margin">
-            <div class="w3-center">
-                <img src="{{ asset('dist/img/logotipo.png') }}" alt="logo" style="width:13%; align=center;">
-
-                <div class="w3-right">
+            <div class="w3-left">
                     <a class="w3-bar-item w3-button" style="margin-top:10px;">
                         <i class="fab fa-twitter-square fa-2x"></i>
                     </a>
@@ -198,6 +195,54 @@
                         <i class="fab fa-facebook-square fa-2x"></i>
                     </a>  
                 </div>
+            <div class="w3-center">
+                <img src="{{ asset('dist/img/logotipo.png') }}" alt="logo" style="width:13%; align=center;">
+
+                <div class="w3-right">
+                    <button class="w3-bar-item w3-button w3-right" style="margin-top:20px;"><i class="fa fa-globe fa-lg"></i>{{ App::getLocale() }}</button>
+                        <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                            <a class="w3-bar-item w3-button" href="/lang/es">@lang('data.español')</a>
+                            <a class="w3-bar-item w3-button" href="/lang/en">@lang('data.ingles')</a>
+                        </div>
+
+                @guest
+
+                    <div class="w3-dropdown-hover w3-right" style="margin-top:24px;">
+                        <button class="w3-button"><i class="fa fa-user fa-lg"></i></button>
+                            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                                <a class="w3-bar-item w3-button" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                @if (Route::has('register'))
+                                    <a class="w3-bar-item w3-button" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                @endif
+                            </div>
+                        </div>
+
+                    @else
+
+                    <div class="w3-dropdown-hover w3-right" style="margin-top:24px;">   
+                        <button class="w3-button">{{ Auth::user()->name }} <span class="caret"></span></button>
+                            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                               
+                                @hasrole('Admin')
+                                    <a class="w3-bar-item w3-button" href="{{ route('admin') }}">Panel de Administrador</a>
+                                @endhasrole
+
+                                @hasrole('comment_admin')
+                                    <a class="w3-bar-item w3-button" href="{{ route('admin') }}">Panel de Administrador</a>
+                                @endhasrole
+
+                                <a class="w3-bar-item w3-button" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                    </div>
+                @endguest
+          
             </div>
         </div>   
     </div>
@@ -240,7 +285,7 @@
     @yield('content')
     </main>
 
-    <div class="footer">
+    <div class="footer" style="padding:3px; background-color:#336699;">
         <div class="w3-center">
             <p><b>Derechos Reservados &copy 2020. Revista Científica. Universidad de Margarita</b></p>
         </div>
