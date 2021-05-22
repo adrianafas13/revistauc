b6c3257e2cc0e218676300fe375e58dc5caacf1f
Adriana <adrianafas13@hotmail.com>
2021-05-22 13:35:02 -0400

    personalizacion del modal login beta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,23 @@
 <style>
-.modal-body{
-    background: #ecf0f1;
+
+.modal-content {
+	padding: 10px;
+	border-radius: 5px;
+	border: none;
+   
+}
+.modal-header .modal-logo img{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 40%;
 }
 .modal-body  .form-group {
 	position: relative;
+}
+.modal-body h6{
+    font-size: 18px;
+    padding-bottom: 6px;
 }
 .modal-body i {
 	position: absolute;
@@ -14,6 +28,10 @@
 .modal-body .form-control {
 	padding-left: 40px;
 }
+
+.modal-body .col-6 a{
+    font-size: 15px;
+}
 </style>
 
 
@@ -22,20 +40,23 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalLogin">
-                    <img src="{{ asset('images/logotipo.png') }}" alt="logo" style="width:30%">
-                    Iniciar Sesión
-                </h5>
+                    <div class="modal-logo" id="modalLogin">   
+                        <img src="{{ asset('images/logotipo.png') }}" alt="logo">
+                    </div>
+                   
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
+                <div class="modal-title">
+                    <h6>Inicia Sesión</h6>
+                </div>
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
                     <div class="form-group">
-						<i class="fa fa-user"></i>
+                        <i class="fas fa-envelope"></i>
 						<input type="email" id="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Correo Electronico">
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -62,18 +83,21 @@
                                 </label>
                         </div>
                     </div>
-
                     <div class="form-group">
-                        <button type="submit" class="btn btn-success btn-block btn-lg">
-                            {{ __('Login') }}
-                        </button>
-
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-outline-dark btn-block btn-lg" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
-                
+                        <div class="row">
+                            <div class="col-6">
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-outline-dark btn-block btn-lg" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                            <div class="col-6">
+                                <a type="submit" class="btn btn-success btn-block btn-lg">
+                                    {{ __('Login') }}
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </form>
 
