0db290b5e43a2823325fea4f8bf004fb816c755b
Adriana <adrianafas13@hotmail.com>
2021-06-01 15:00:36 -0400

    personalizacion del modal de autores

--- resources/views/modalAuthors.blade.php
+++ resources/views/modalAuthors.blade.php
@@ -1,16 +1,33 @@
 <!-- Modal -->
 <div class="modal fade" id="authorsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable">
+  <div class="modal-dialog modal-dialog-scrollable">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+
+      <div class="modal-author-content">
+        <div class="modal-header">
+          <div class="modal-title">
+            <p><b>Autores por la letra</b></p>
+          </div>
+
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+
+        <div class="modal-body">
+
+          <div class="modal-info-author">
+            <div class="modal-author-name">
+              <p><b>Nombre y Apellido del Autor</b></p>
+            </div>
+            <div class="modal-author-article">
+              <p>Título completo de los articulos publicados por el autor en la revista cientifica de la universidad de margarita</p>
+            </div>
+          </div>
+
+        </div>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
+
     </div>
   </div>
 </div>
