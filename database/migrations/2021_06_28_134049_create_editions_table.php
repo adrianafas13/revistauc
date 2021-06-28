af1feff315e4a034f9494942c83e937e7e0b2b0a
Adriana <adrianafas13@hotmail.com>
2021-06-28 14:09:39 -0400

    correccion de la base de datos de las ediciones

--- database/migrations/2021_06_28_134049_create_editions_table.php
+++ database/migrations/2021_06_28_134049_create_editions_table.php
@@ -15,9 +15,18 @@ class CreateEditionsTable extends Migration
     {
         Schema::create('editions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            /*spanish*/
             $table->text('route_image_edition');
             $table->string('title_edition',1500)->nullable(false);
             $table->integer('number_edition')->nullable(false);
+            /*english*/
+            $table->text('en_route_image_edition');
+            $table->string('en_title_edition',1500)->nullable(false);
+            $table->integer('en_number_edition')->nullable(false);
+            /*italian*/
+            $table->text('it_route_image_edition');
+            $table->string('it_title_edition',1500)->nullable(false);
+            $table->integer('it_number_edition')->nullable(false);
             $table->timestamps();
         });
     }
