3447dcd9040d16e22aca29c0a0d1baa2094e875e
Adriana <adrianafas13@hotmail.com>
2021-06-28 13:47:32 -0400

    creacion de la base de datos de las ediciones

---
+++ database/migrations/2021_06_28_134049_create_editions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEditionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('editions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('route_image_edition');
+            $table->string('title_edition',1500)->nullable(false);
+            $table->integer('number_edition')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('editions');
+    }
+}
