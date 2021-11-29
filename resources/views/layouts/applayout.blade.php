aabf02854cdc845f51873eb898bbb95034062241
Adriana <adrianafas13@hotmail.com>
2021-11-29 12:51:35 -0400

    retoques

--- resources/views/layouts/applayout.blade.php
+++ resources/views/layouts/applayout.blade.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Unimar Científica @yield('co-title')</title>
-        <link  rel="icon" href="/images/unimar-científica.png" type="image/png"/>
+        <link  rel="icon" href="/images/rcu-orange-isotype.png" type="image/png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.applayout')
- 
 @section('content')
 <div class="main">
         <br>
@@ -8,39 +7,32 @@
         <h4><b>@lang('data.resultados')</b></h4>
     </div>
     <hr>
-
-    <div class="row">
-        <div class="col-12 .col-sm-12 col-md-6 .col-lg-6 .col-xl-6">
-            <div class="container mt-5">
-                <!-- articulo -->
-                @foreach($articles as $article)
-                <div class="card mb-3">
-                    <div class="row no-gutters">
-
-                        <div class="col-md-4">
-                            <a href="{{route('art', $article->slug)}}">
-                                <img src="/images/{{$article->ruta_image}}" class="img-fluid">
-                            </a>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h6><a href="#" class="badge">{{ $article->section }}</a></h6>
-                                <h5><b>{{ App::isLocale('es')?$article->title:$article->en_title }}</b></h5>
-                                <p>{{ $article->author->name_author }}</p>
-                                <a type="button" class="btn btn-outline-dark" href="{{route('art', $article->slug)}}">@lang('data.ver_mas')</a>
-                            </div>
+    <div class="container mt-5">
+        <!-- articulo -->
+        @foreach($articles as $article)
+            <div class="card mb-3">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <a href="{{route('art', $article->slug)}}">
+                            <img src="/images/{{$article->ruta_image}}" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h6><a href="#" class="badge">{{ $article->section }}</a></h6>
+                            <h5><b>{{ App::isLocale('es')?$article->title:$article->en_title }}</b></h5>
+                            <p>{{ $article->author->name_author }}</p>
+                            <a type="button" class="btn btn-outline-dark" href="{{route('art', $article->slug)}}">@lang('data.ver_mas')</a>
                         </div>
                     </div>
                 </div>
-                @endforeach
-                <br>
             </div>
+            @endforeach
+            <br>
+        </div>
+        <div class="personalize-pagination" style="display:flex;justify-content:center;"> 
+            {{ $articles->links() }}
         </div>
     </div>
-  
-    <div class="personalize-pagination" style="display:flex;justify-content:center;"> 
-    {{ $articles->links() }}
-    </div>
-</div>
 </div>
 @endsection
